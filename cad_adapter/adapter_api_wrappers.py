d402225a89ad3f759aebe3d9291e7fb542426bde
Michael Brunner <brunner@cadwork.swiss>
2025-05-19 14:50:25 +0200

    Remove unused function implementations and raise NotImplementedError for unimplemented methods in adapter_api_wrappers

--- cad_adapter/adapter_api_wrappers.py
+++ cad_adapter/adapter_api_wrappers.py
@@ -1,94 +1,70 @@
 from typing import List
 
-import attribute_controller as ac
 import cadwork
-import element_controller as ec
-import geometry_controller as gc
-import visualization_controller as vc
+
 
 def filter_slab_element_id(elements: List[int]) -> int:
-    # raise NotImplementedError()
-    return next(filter(lambda x: ac.get_element_type(x).is_floor(), elements), None)
+    raise NotImplementedError()
 
 
 def move_point(point: cadwork.point_3d, direction_vector: cadwork.point_3d,
                distance: float) -> cadwork.point_3d:
-    new_point = point + direction_vector * distance
-    return new_point
-    # raise NotImplementedError()
+    raise NotImplementedError()
 
 
 def get_active_elements() -> List[int]:
-    return ec.get_active_identifiable_element_ids()
-    # raise NotImplementedError()
+    raise NotImplementedError()
 
 
 def get_element_width(element_id: int) -> float:
-    return gc.get_width(element_id)
-    # raise NotImplementedError()
+    raise NotImplementedError()
 
 
 def get_element_height(element_id: int) -> float:
-    return gc.get_height(element_id)
-    # raise NotImplementedError()
+    raise NotImplementedError()
 
 
 def get_element_p1(element_id: int) -> cadwork.point_3d:
-    return gc.get_p1(element_id)
-    # raise NotImplementedError()
+    raise NotImplementedError()
 
 
 def get_element_p2(element_id: int) -> cadwork.point_3d:
-    return gc.get_p2(element_id)
-    # raise NotImplementedError()
+    raise NotImplementedError()
+
 
 def get_element_p3(element_id: int) -> cadwork.point_3d:
-    return gc.get_p3(element_id)
-    # raise NotImplementedError()
+    raise NotImplementedError()
+
 
 def get_element_yl(element_id: int) -> cadwork.point_3d:
-    return gc.get_yl(element_id)
-    # raise NotImplementedError()
+    raise NotImplementedError()
 
 
 def get_element_zl(element_id: int) -> cadwork.point_3d:
-    return gc.get_zl(element_id)
-    # raise NotImplementedError()
+    raise NotImplementedError()
 
 
 def create_node(point: cadwork.point_3d) -> int:
-    return ec.create_node(point)
-    # raise NotImplementedError()
+    raise NotImplementedError()
 
 
 def create_rectangular_beam(start_point: cadwork.point_3d, end_point: cadwork.point_3d, width: float,
                             height: float,
                             height_axis_orientation: cadwork.point_3d
                             ) -> int:
+    # TODO: implement creation of beam. p3 is e.g. start point + direction
 
-    #TODO: implement creation of beam. p3 is e.g. start point + direction
+    raise NotImplementedError()
 
-    return ec.create_rectangular_beam_points(width,
-                                             height,
-                                             start_point,
-                                             end_point,
-                                             start_point + height_axis_orientation)
-    # raise NotImplementedError())
 
 def create_rectangular_panel(width: float, thickness: float, p1: cadwork.point_3d,
                              p2: cadwork.point_3d, p3: cadwork.point_3d) -> int:
-    return ec.create_rectangular_panel_points(width,
-                                                  thickness,
-                                                  p1,
-                                                  p2,
-                                                  p3)
-    # raise NotImplementedError()
+    raise NotImplementedError()
+
 
 def set_color(elements: List[int], color: int):
-    vc.set_color(elements, color)
-    # raise NotImplementedError()
+    raise NotImplementedError()
 
 
 def set_name(elements: List[int], name: str):
-    ac.set_name(elements, name)
-    # raise NotImplementedError()
+    raise NotImplementedError()
